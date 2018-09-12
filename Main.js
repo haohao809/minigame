cb7f6ce3da6ebf936a2c8f7b3d72b7e3f5243df6
安志浩 <haohao809@126.com>
2018-09-12 15:20:07 +0800

    渲染背景图

--- Main.js
+++ Main.js
@@ -1,5 +1,5 @@
 import {ResourceLoader} from './js/base/ResourceLoader.js';
-import {Director} from './js/Director.js';
+import {BackGround} from './js/runtime/BackGround.js';
 export class Main{
 	constructor() {
 		// console.log('我的游戏');
@@ -7,11 +7,13 @@ export class Main{
 		this.canvas = document.getElementById('game_canvas');
 		this.ctx = this.canvas.getContext('2d');
 		const loader = ResourceLoader.create();
-		loader.onLoaded(map => this.onResourceFirstLoaded(map))
+		loader.onLoaded(map => this.onResourceFirstLoaded(map));
 
-		Director.getInstance(); 
+
 	}
 	onResourceFirstLoaded(map) {
-		console.log(map)
+		console.log(map);
+		let background = new BackGround(this.ctx,map.get('background'));
+		background.draw();
 	}
 }
