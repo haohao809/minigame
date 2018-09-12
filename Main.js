7dcd5a5e26356f37b3d1437612482f1643d252fa
安志浩 <haohao809@126.com>
2018-09-12 12:02:13 +0800

    图片加载

--- Main.js
+++ Main.js
@@ -1,5 +1,17 @@
+import {ResourceLoader} from './js/base/ResourceLoader.js';
+import {Director} from './js/Director.js';
 export class Main{
 	constructor() {
-		console.log('我的游戏')
+		// console.log('我的游戏');
+		// new ResourceLoader();
+		this.canvas = document.getElementById('game_canvas');
+		this.ctx = this.canvas.getContext('2d');
+		const loader = ResourceLoader.create();
+		loader.onLoaded(map => this.onResourceFirstLoaded(map))
+
+		Director.getInstance(); 
+	}
+	onResourceFirstLoaded(map) {
+		console.log(map)
 	}
 }
